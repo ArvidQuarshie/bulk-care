6389a75a5462257853a5ceee0b4a8aaa6cde0693
arvid <arvidquarshie@gmail.com>
2025-06-08 19:13:57 +0300

    Updated ValidationResults.tsx

--- src/components/ValidationResults.tsx
+++ src/components/ValidationResults.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ValidationResult, ValidationSummary as ValidationSummaryType } from '../types';
 import ValidationItem from './ValidationItem';
 import ValidationSummary from './ValidationSummary';
